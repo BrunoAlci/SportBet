4eb7906d23b15a178020c3d2725e67dcc242bc29
erickcaceres <katomax1@gmail.com>
2022-03-14 17:10:50 -0500

    avance 14/04/2022

--- nav.js
+++ nav.js
@@ -1,6 +1,24 @@
-const menuBar = document.querySelector(menu_bar)
-const ulResponsive = document.querySelector(ul-responsive)
+$(document).ready(main);
 
-menubar.addEventListener("click", () =>{
-menuresponsive.classList.toggle("menu-responsive:hover")
-})
+var contador = 1;
+
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
