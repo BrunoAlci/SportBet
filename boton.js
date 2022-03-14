f67166988f01852330b3a4a0594ea2be85d5763b
erickcaceres <katomax1@gmail.com>
2022-03-14 11:26:48 -0500

    avance inicial

--- boton.js
+++ boton.js
@@ -1,5 +1,6 @@
 
 
+
 // function geek() {
   document.querySelectorAll('a[href^="#ancla1"]').forEach(elem => {
     elem.addEventListener('click', e => {
---
+++ nav.js
@@ -0,0 +1,6 @@
+const menuBar = document.querySelector(menu_bar)
+const ulResponsive = document.querySelector(ul-responsive)
+
+menubar.addEventListener("click", () =>{
+menuresponsive.classList.toggle("menu-responsive:hover")
+})
