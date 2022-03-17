f39ff3f8cede8edd2632d26044e4e516c4b95f64
erickcaceres <katomax1@gmail.com>
2022-03-17 17:10:20 -0500

    nav terminado

---
+++ contact.js
@@ -0,0 +1,32 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main () {
+	$('.img-contact-form').click(function(){
+		if (contador == 1) {
+			$('.contact_form').animate({
+				right: '-1rem'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.contact_form').animate({
+				right: '100%'
+			});
+		}
+	});
+	$('.contact-escape').click(function(){
+		if (contador == 1) {
+			$('.contact_form').animate({
+				right: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.contact_form').animate({
+				right: '100%'
+			});
+		}
+	});
+}
